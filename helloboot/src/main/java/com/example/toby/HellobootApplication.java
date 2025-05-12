b4ad2b67eca2a36a548e6085b4064860da99fe30
sojeong <th21@naver.com>
2025-05-12 19:37:59 +0900

    project 구조 변경

--- helloboot/src/main/java/com/example/toby/HellobootApplication.java
+++ helloboot/src/main/java/com/example/toby/HellobootApplication.java
@@ -1,4 +1,4 @@
-package tobyspring.helloboot;
+package com.example.toby;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class HellobootApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(HellobootApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(HellobootApplication.class, args);
+    }
 
 }
